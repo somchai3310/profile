1491b84bdf5d8c92bfb809bdabf7c3bfc0089a6f
Somchai3310 <somchaibefore@gmail.com>
2021-08-12 03:24:19 +0700

    first edited

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <template>
-  <div id="app">Hello Mustofa</div>
+  <div id="app" class="flex h-screen">
+    <div class="profile bg-blue-200 p-10 rounded-xl shadow-2xl m-auto text-center w-80 animate-pulse">
+      <img
+        src="https://scontent.fcnx2-1.fna.fbcdn.net/v/t31.18172-8/12265944_982653401778485_3423967004235640155_o.jpg?_nc_cat=107&ccb=1-4&_nc_sid=cdbe9c&_nc_ohc=Zj0Xfk88dE0AX-dmkvl&_nc_ht=scontent.fcnx2-1.fna&oh=af7fb7a5b940013d1b287a1f0598b665&oe=61397361"
+        alt=""
+        class="w-40 h-40 object-cover rounded-full shadow-xl hover:shadow-2xl mx-auto mb-6"
+      />
+    <h1 class="text-xl">Hello Mustofa</h1>
+    <p class="text-gray-500">I am Frontend Developer.</p>
+    </div>
+  </div>
 </template>
 
 <script>
